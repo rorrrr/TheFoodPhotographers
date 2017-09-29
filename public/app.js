4771dd988c4e2e5065464ebd29755124b0e32e35
rorrrr <rorymacdonald6@hotmail.com>
2017-09-29 15:39:07 +0100

    working transition, image-gallery, no landing x

--- public/app.js
+++ public/app.js
@@ -1,14 +1,21 @@
 console.log('sup');
 
-var imageCount = 1;
-var total = 4;
+// var imageCount = 1;
+// var total = 4;
 
+// window.setInterval(function photoA() {
+//   var body = document.getElementById('body');
+//   imageCount = imageCount + 1;
+//   if(imageCount > total){imageCount = 1;}
+//   body.style= "background: url('Food"+ imageCount +".jpg') no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover -o-background-size: cover; background-size: cover; -webkit-transition: opacity 1s ease-in-out; -moz-transition: opacity 1s ease-in-out; -o-transition: opacity 1s ease-in-out transition: opacity 1s ease-in-out;"
+// }, 8000)
+// // )
 
-window.setInterval(function photoA() {
-  var body = document.getElementById('body');
-  imageCount = imageCount + 1;
-  if(imageCount > total){imageCount = 1;}
-  body.style= "background: url('Food"+ imageCount +".jpg') no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover -o-background-size: cover; background-size: cover; animation: 2s ease-in-out"
-}, 5000)
-// )
-
+var images = [
+  {image: 'Food1.jpg'},
+  {image: 'Food2.jpg'},
+  {image: 'Food3.jpg'},
+  {image: 'Food3.jpg'}
+];
+ 
+bgImageGallery(images, 'body', 8000, false);
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var backgroundImageGallery = require("background-image-gallery");
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
