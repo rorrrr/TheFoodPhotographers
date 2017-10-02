8c0fe36a7baee14860c6a60fc786ac18db0ee8e4
rorrrr <rorymacdonald6@hotmail.com>
2017-10-02 16:59:38 +0100

    functional site, addedfonts, coverage client contact

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-console.log('sup');
+console.log('application ran');
 
 // var imageCount = 1;
 // var total = 4;
@@ -11,11 +11,47 @@ console.log('sup');
 // }, 8000)
 // // )
 
+window.onload = function () { document.body.className = ""; }
+
 var images = [
-  {image: 'Food1.jpg'},
-  {image: 'Food2.jpg'},
-  {image: 'Food3.jpg'},
-  {image: 'Food3.jpg'}
+{image: 'Food2.jpg'},
+{image: 'Food1.jpg'},
+{image: 'Food3.jpg'},
+{image: 'Food4.jpg'}
 ];
- 
+
 bgImageGallery(images, 'body', 8000, false);
+
+
+var landing = document.getElementById("central");
+var coverageContent = document.getElementById("coverageContent");
+var clients = document.getElementById("clients");
+var contact = document.getElementById("contact");
+
+function coverageShow(){
+  landing.style.display = "none";
+  coverageContent.style.display = "block";
+  contactContent.style.display = "none";
+  clientsContent.style.display = "none";
+  console.log('coverageclick');
+}
+
+function clientsShow()
+{
+  landing.style.display = "none";
+  coverageContent.style.display = "none";
+  contactContent.style.display = "none";
+  clientsContent.style.display = "block";
+  console.log('coverageclick');
+}
+
+function contactShow()
+{
+  landing.style.display = "none";
+  coverageContent.style.display = "none";
+  contactContent.style.display = "block";
+  clientsContent.style.display = "none";
+  console.log('coverageclick');
+}
+
+
